4a3c8f9bc3e252bddb23b7313c8e8624afb05bd5
duke <dukeluo@outlook.com>
2020-04-16 22:24:28 +0800

    update blog

--- js/cover.js
+++
@@ -1,7 +0,0 @@
-;(function ($) {
-  var $banner = $('#cover #banner');
-  var bannerUrls = $banner.attr("data-banners").split(';');
-  var randomBannerUrl = bannerUrls[Math.floor(Math.random() * bannerUrls.length)];
-
-  $banner.css('background-image', 'url(' + randomBannerUrl + ')');
-})(jQuery);
