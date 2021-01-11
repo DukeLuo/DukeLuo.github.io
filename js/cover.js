5356ff71bdc11c657123c1afc1ebcebc55c8723d
duke <dukeluo@outlook.com>
2021-01-11 22:46:33 +0800

    update blog

--- js/cover.js
+++ js/cover.js
@@ -1,9 +1,7 @@
-// eslint-disable-next-line no-extra-semi
-;(function ($) {
-  var $banner = $('#cover #banner');
-  var bannerUrls = $banner.attr('data-banners').split(';');
-  var randomBannerUrl = bannerUrls[Math.floor(Math.random() * bannerUrls.length)];
+(function ($) {
+  let $banner = $('#cover #banner');
+  let bannerUrls = $banner.attr('data-banners').split(';');
+  let randomBannerUrl = bannerUrls[Math.floor(Math.random() * bannerUrls.length)];
 
   $banner.css('background-image', 'url(' + randomBannerUrl + ')');
-// eslint-disable-next-line no-undef
 })(jQuery);
